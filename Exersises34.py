34890a5f14f7804d73163e4b4811d3879acde29e
ElonTusk404 <igmaflappy@gmail.com>
2023-06-20 19:55:36 +0400

    Create Exersises34.py

---
+++ Exersises34.py
@@ -0,0 +1,13 @@
+animals = ['медведь', 'питон', 'пингвин', 'кенгуру', 'кит', 'утконос']
+print(f"Животное с индексом 1: {animals[1]}")
+print(f"Третье животное: {animals[2]}")
+print(f"Первое животное: {animals[0]}")
+print(f"Животное с индексом 3: {animals[3]}")
+print(f"Пятое животное: {animals[4]}")
+print(f"Животное с индексом 2: {animals[2]}")
+print(f"Шестое животное: {animals[5]}")
+print(f"Животное с индексом 4: {animals[4]}")
+companies = ['Google', 'Apple', 'Microsoft', 'Amazon', 'Facebook', 'Netflix']
+print(f"Последняя компания: {companies[-1]}")
+print(f"Первая компания: {companies[0]}")
+print(f"Компания с интексом 5: {companies[5]}")
